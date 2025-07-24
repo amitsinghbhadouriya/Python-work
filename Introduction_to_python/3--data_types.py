5e39b6bd8f9d5d2cb5ca10aae289123906dfe907
Amit Singh <abhaybhadouriya88@gmail.com>
2025-07-25 02:47:17 +0530

    Add data_types.py

---
+++ Introduction_to_python/3--data_types.py
@@ -0,0 +1,26 @@
+'''Data Types'''
+
+# 1. Integers
+num1 = 23
+print(type(num1))
+
+
+# 2. String
+name1 = 'Amit'           # Single qoute
+name2 = "Amit"           # Double qoute
+name3 = '''Amit'''       # Triple qoute
+print(type(name1))
+print(type(name2))
+print(type(name3))
+
+# 3. Float
+num2 = 23.45
+print(type(num2))
+
+# 4. Boolean
+Happy = False
+print(type(Happy))
+
+# 5. None
+A = None
+print(type(A))
