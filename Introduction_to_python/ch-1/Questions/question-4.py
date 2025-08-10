3b6fd1d93015c4f4d0b5b3f43ebef239defad5f9
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-10 22:07:49 +0530

    add question 4

---
+++ Introduction_to_python/ch-1/Questions/question-4.py
@@ -0,0 +1,8 @@
+# Write a program to input 2 int numbers, a and b , Print True if a is greater than or equal to b, If not print false.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if(a>=b):
+    print(True)
+else:
+    print(False)
