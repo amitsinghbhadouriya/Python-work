638b27a9000e9e13661c3816db93396fd7f6cf3c
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-10 22:04:17 +0530

    add question-3

---
+++ Introduction_to_python/ch-1/Questions/question-3.py
@@ -0,0 +1,6 @@
+# Write a program to input 2 floating point number and print their average.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+Average = (num1 + num2)/2
+print("The average of two floating point number is", Average)
