5b68c6f687334d103a9a4c16fb45c81fa4e27165
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-11 21:08:07 +0530

    add strings

---
+++ Introduction_to_python/ch-2/1--string.py
@@ -0,0 +1,16 @@
+str1 = 'Amit'                   # Single quoted string
+str2 = "Amit"                   # Double quoted string
+str3 = """Amit"""               # Triple quoted string
+
+# Escape Sequence Character
+a = "This is a string. \nI write string in python."
+print(a)
+
+# Concatenation of string
+b = "Amit"
+c = " Singh"
+d = b + c
+print(d)
+
+# Length of a string
+print(len(d))
