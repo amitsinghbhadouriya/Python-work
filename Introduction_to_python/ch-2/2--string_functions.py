ed50d5e9b46a96b566300b4b7272c0fdd0e0fe01
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-11 21:35:41 +0530

    add string functions

---
+++ Introduction_to_python/ch-2/2--string_functions.py
@@ -0,0 +1,22 @@
+# Concatenation of string
+b = "Amit"
+c = " Singh"
+d = b + c
+print(d)
+
+# Length of a string
+print(len(d))
+
+# count of a string
+str = "eurogames"
+print(str.count("e"))
+
+# Capitalize of a string
+print(str.capitalize())
+
+# Find in a string
+print(str.find("games"))
+
+# replace string
+str4 = "I like java."
+print(str4.replace("java", "python"))
