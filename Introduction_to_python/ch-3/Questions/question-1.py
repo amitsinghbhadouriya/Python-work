7e179769291eca7d54ea871b245b0b4a067e380c
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-12 22:55:28 +0530

    add question 1

---
+++ Introduction_to_python/ch-3/Questions/question-1.py
@@ -0,0 +1,8 @@
+# Write a program to ask the user to enter names of their 3 favorite movies and store them in a list.
+
+movie1 = input("Enter first movie: ")
+movie2 = input("Enter second movie: ")
+movie3 = input("Enter third movie: ")
+
+movies = [movie1, movie2, movie3]
+print(movies)
