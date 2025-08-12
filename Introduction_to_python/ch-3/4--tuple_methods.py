7640d96953f17db4aad755459b1dc2a88266b3ae
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-12 21:29:29 +0530

    add tuple methods

---
+++ Introduction_to_python/ch-3/4--tuple_methods.py
@@ -0,0 +1,14 @@
+# tuple methods
+
+# 1. concatenation 
+tup1 = (12, 45)
+tup2 = ("Hello", "world")
+tup3 = tup1 + tup2
+print(tup3)
+
+# 2. count
+num = (1, 4, 5, 1, 8, 9, 1, 19, 32, 1)
+print(num.count(1))
+
+# 3. index
+print(num.index(5))
