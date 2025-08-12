b52b41707a1cc6fb2a28313e1ce914b6a7487d28
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-12 21:10:42 +0530

    add tuple

---
+++ Introduction_to_python/ch-3/3--tuple.py
@@ -0,0 +1,17 @@
+# Tuples
+tuple = (24, 45, 8, 19)
+print(tuple)
+
+# type of tuple
+print(type(tuple))
+
+# access tuple by indexing
+print(tuple[1])
+print(tuple[3])
+
+# access tuple by slicing
+print(tuple[1:len(tuple)])
+
+# access tuple using loop
+for item in tuple:
+    print(item)
