006e1e46642a448acbfe5c9e20477e3bc0bd6b03
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-12 20:16:17 +0530

    add list

---
+++ Introduction_to_python/ch-3/1--list.py
@@ -0,0 +1,18 @@
+# list
+student = ["Amit", 20, 98.4, "Gwalior", "Amity"]
+print(student)
+
+# Type of list
+print(type(student))
+
+# len of list
+print(len(student))
+
+# Indexing of list
+print(student[0])
+print(student[4])
+print(student[2])
+
+# list slicing
+print(student[1:4])
+print(student[0:len(student)])
