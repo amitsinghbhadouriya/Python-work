e38ce9337027316903d9d661f9c1f1b99273c17a
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-12 20:26:02 +0530

    add list methods

---
+++ Introduction_to_python/ch-3/2--list_methods.py
@@ -0,0 +1,28 @@
+# list Methods
+
+list = [2,6,5,29,4,17]
+print(list)
+
+# 1. sort
+list.sort()
+print(list)
+
+# 2. reverse
+list.reverse()
+print(list)
+
+# 3. Append
+list.append(8)
+print(list)
+
+# 4. insert
+list.insert(3,1)
+print(list)
+
+# 5. pop
+list.pop(1)
+print(list)
+
+# 6. remove
+list.remove(1)
+print(list)
