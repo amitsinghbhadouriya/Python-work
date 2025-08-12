f952747de6e3f93f8df5cbe93c543825d0f0df7c
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-12 23:09:44 +0530

    add question 2

---
+++ Introduction_to_python/ch-3/Questions/question-2.py
@@ -0,0 +1,10 @@
+# (i) Write a program to count the number of students with the "A" grade in the following tuple.
+# ("C","D","A","A","B","B","A")
+
+# (ii) Store the above values in a list and sort them from "A" to "D"
+tuple = ("C","D","A","A","B","B","A")
+print(tuple.count("A"))
+
+list = ["C","D","A","A","B","B","A"]
+list.sort()
+print(list)
