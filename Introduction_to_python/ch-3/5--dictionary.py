75a7c3ceee6e6060978473fe7c43a31bb14efadc
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-13 21:22:55 +0530

    add dictionary

--- Introduction_to_python/ch-3/5--dictionary.py
+++ Introduction_to_python/ch-3/5--dictionary.py
@@ -9,3 +9,19 @@ print(dict["age"])
 
 # type of dictionaries
 print(type(dict)) 
+
+# Nested dictionaries
+dict = {
+    "name" : "Amit",
+    "age" : 19, 
+    "cgpa" : 8.9,
+    "Course" : "BCA",
+    "Subject-Grade" : {
+        "WDD" : "O",
+        "Maths" : "A+",
+        "COF" : "A+",
+        "C++" : "A",
+        "CO" : "B+"
+    }
+}
+print(dict)
