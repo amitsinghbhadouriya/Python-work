c57116de18fd12210ca59b06e8f788b649dbcc7a
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-13 22:04:46 +0530

    add set_methods

---
+++ Introduction_to_python/ch-3/8--set_methods.py
@@ -0,0 +1,24 @@
+# Start with a list
+s = [4,1,3,8,5,2,9,6]
+
+# 1. length
+print(len(s)) 
+
+# Convert to set
+s = set(s)
+
+# 2. union
+s = s.union({12, 45})
+print("After union:", s)
+
+# 3. intersection (store result)
+s = s.intersection({9, 3})
+print("After intersection:", s)
+
+# 4. remove
+s.remove(3)  # removes element 3 if exists
+print("After remove:", s)
+
+# 5. clear
+s.clear()
+print("After clear:", s)
