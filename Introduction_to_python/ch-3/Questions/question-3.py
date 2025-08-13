15150a829f41c6d43cbccf1c0d9c7daa86f8412f
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-13 22:20:02 +0530

    add question-3

---
+++ Introduction_to_python/ch-3/Questions/question-3.py
@@ -0,0 +1,9 @@
+# Store following word meanings in a python dictionary:
+# table: "a piece of furniture", "list of facts and figures"
+# cat: "a small animal"
+
+dict = {
+    "table" : ["a piece of furniture", "list of facts and figures"],
+    "cat" : "a small animal"
+}
+print(dict)
