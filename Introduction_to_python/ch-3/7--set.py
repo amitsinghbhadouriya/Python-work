f19689b94e0cd65a9df15359d7110065b9874c72
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-13 21:43:38 +0530

    add set

---
+++ Introduction_to_python/ch-3/7--set.py
@@ -0,0 +1,22 @@
+# Set
+
+a = {1,2,3,4}
+print(a)
+
+# Accessing the type of a set
+print(type(a))   
+
+# Creating a set using a set function
+set1 = set()
+print(set1)
+
+set1 = set("hello world")
+print(set1)
+
+# Creating a set with the help of a list
+set1 = set(["Hello", "World", "Hello"])
+print(set1)
+
+# Creating a set with the help of a tuple
+tup = ("Hello", "World", "Hello")
+print(set(tup))
