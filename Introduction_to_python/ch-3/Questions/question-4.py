d55887ca231b56f5515e35131706dc8dc6964825
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-13 22:26:32 +0530

    add question-4

---
+++ Introduction_to_python/ch-3/Questions/question-4.py
@@ -0,0 +1,6 @@
+# You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.
+# "python", "java", "C++", "python", "javaScript", "java", "python", "java", "C++", "C"
+
+set = {"python", "java", "C++", "python", "javaScript", "java", "python", "java", "C++", "C"}
+print(set)
+print(len(set))
