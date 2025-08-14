342d6c155529bd501ac39a10bac4e84931f946a5
Amit Singh <abhaybhadouriya88@gmail.com>
2025-08-14 20:02:28 +0530

    add loops

--- Introduction_to_python/ch-1/6--loops.py
+++ Introduction_to_python/ch-1/6--loops.py
@@ -10,3 +10,24 @@ n = int(input("Enter the number: "))
 while(n < 5):
     n = n + 1
     print("Hello World")
+    
+# Break
+i = 1
+while(i <= 8):
+    print(i)
+    if(i==4):
+        break
+    i += 1
+    
+print("loop is ended.")
+
+# Continue
+i = 1
+while(i <= 8):
+    if(i==4):
+        i += 1
+        continue
+    print(i)
+    i += 1
+    
+print("loop is ended.")
