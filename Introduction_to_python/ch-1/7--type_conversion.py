d525bf781ad36575f4ecf1149c05e3e3a085f2b0
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-11 19:10:58 +0530

    add file to ch-1

---
+++ Introduction_to_python/ch-1/7--type_conversion.py
@@ -0,0 +1,20 @@
+# Implicit Type Conversion
+x = 10      
+y = 2.5     
+z = x + y  
+print(z)
+print(type(z))
+
+# Explicit Type Conversion
+a = int(3.8)
+b = float(5)  
+c = str(100)     
+d = bool(1)       
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
