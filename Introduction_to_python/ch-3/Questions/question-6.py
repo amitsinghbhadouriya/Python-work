63887c14f668b0757b1a121ee346287527163225
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-15 12:25:41 +0530

    add new question to ch-3 questions of itp

---
+++ Introduction_to_python/ch-3/Questions/question-6.py
@@ -0,0 +1,8 @@
+# Figure out a way to store 9 and 9.0 as separate values in the set.
+# (You can take help of built-in data types)
+
+values = {
+    ("int", 9),
+    ("float", 9.0)
+}
+print(values)
