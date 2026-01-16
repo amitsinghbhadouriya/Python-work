3ba1167fe42bdfa6cf6a2ed7d96361ed62fae882
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-16 23:00:27 +0530

    add question to ch-4 questions of itp

---
+++ Introduction_to_python/ch-4/Questions/question-3.py
@@ -0,0 +1,9 @@
+# Write a function to find the factorial of n. (n is the parameter)
+
+def cal_fact(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact = fact * i
+    print(fact)
+    
+cal_fact(5)
