fb43ccee727b0f00b723a02e0c74d4c797e33bb2
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-16 22:48:12 +0530

    add question to ch-4 questions of itp

---
+++ Introduction_to_python/ch-4/Questions/question-1.py
@@ -0,0 +1,10 @@
+# Write a function to print the length of a list. (list is the parameter)
+
+Countries = ['India', 'US', 'UK', 'Russia', 'Australia', 'South Africa', 'Vietnam']
+cities = ['Delhi', 'Mumbai', 'Bangalore', 'Pune']
+
+def print_len(list):
+    print(len(list))
+    
+print_len(Countries)
+print_len(cities)
