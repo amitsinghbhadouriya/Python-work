903a40ee9882acde1acb6c5d3e0af69c5e3bc500
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-17 23:22:03 +0530

    add question to ch-5 questions of itp

---
+++ Introduction_to_python/ch-5/Questions/question-1.py
@@ -0,0 +1,8 @@
+# Create a new file "practice.txt" using python. Add the following data in it:
+# Hi everyone
+# we are learning file I/O
+# using Python.
+# I like programming in Python.
+
+with open("practice.txt", "w") as f:
+    f.write("Hi everyone \nwe are learning file I/O \nusing Python. \nI like programming in Python.")
