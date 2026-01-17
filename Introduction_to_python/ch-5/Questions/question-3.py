5d7518d3279fa202b55d8b5c41baf0c5cb782395
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-17 23:44:58 +0530

    add question to ch-5 questions of itp

--- Introduction_to_python/ch-5/Questions/question-3.py
+++ Introduction_to_python/ch-5/Questions/question-3.py
@@ -1,1 +1,9 @@
 # Search if the word "learning" exists in the file or not.
+
+word = "learning"
+with open(r"Introduction_to_python\ch-5\Questions\practice.txt") as f:
+    data = f.read()
+    if(data.find(word) != -1):
+        print("Found")
+    else:
+        print("not found")
