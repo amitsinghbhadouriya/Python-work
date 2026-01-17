96799793482bda40cd6f020a87ce1859a920beca
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-17 23:30:44 +0530

    add question to ch-5 questions of itp

--- Introduction_to_python/ch-5/Questions/question-2.py
+++ Introduction_to_python/ch-5/Questions/question-2.py
@@ -1,5 +1,5 @@
 with open(r"Introduction_to_python\ch-5\Questions\practice.txt","r") as f:
     data = f.read()
     
-data.replace("Python", "Java")
-print(data)
+new_data = data.replace("Python", "Java")
+print(new_data)
