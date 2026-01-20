09bdc783cb8ed8f6f98af5acb4ec95257b5e4707
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-20 18:03:51 +0530

    add file to ch-5 of itp

---
+++ Introduction_to_python/ch-5/1--file.py
@@ -0,0 +1,5 @@
+# Writing to a file
+
+File = open(r"Introduction_to_python\ch-5\sample.txt","w")
+File.write("Hello Python \nThis a sample document")
+File.write("\nPython is an interpreted language \nIt is easy to learn and understand")
